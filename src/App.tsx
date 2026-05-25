8d5179bbffd4a59121d61cfa9e6b40a1715affea
kiranfin <4finjas@online.de>
2026-05-25 12:46:02 +0200

    Split App.tsx in AppRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
-import { Route, Routes } from 'react-router-dom';
+import { AppRouter } from './router/AppRouter';
 import { SideBar } from './components/layout/SideBar';
-import { ChatPage } from './pages/ChatPage';
-import { HomePage } from './pages/HomePage';
-import { KnowledgeBasePage } from './pages/KnowledgeBasePage';
-import { OnBoardingPage } from './pages/OnBoardingPage';
 
 function App() {
   return (
       <div className="flex min-h-screen w-full bg-gray-950">
         <SideBar />
 
-        <main className="min-h-screen flex-1 bg-gray-950">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/chat" element={<ChatPage />} />
-            <Route path="/onboarding" element={<OnBoardingPage />} />
-            <Route path="/knowledge-base" element={<KnowledgeBasePage />} />
-          </Routes>
+        <main className="min-h-screen flex-1 bg-gray-950 pt-[64px] lg:pt-0">
+          <AppRouter />
         </main>
       </div>
   );
