b7da394eb6ba95881a53b1eefe2eab79480d17a6
kiranfin <4finjas@online.de>
2026-06-01 19:13:59 +0200

    Renamed Home to Dashboard

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import { ChatPage } from '../pages/ChatPage';
-import { HomePage } from '../pages/HomePage';
+import { DashboardPage } from '../pages/DashboardPage.tsx';
 import { KnowledgeBasePage } from '../pages/KnowledgeBasePage';
 import { OnBoardingPage } from '../pages/OnBoardingPage';
 import { LoginPage } from '../pages/LoginPage';
@@ -13,7 +13,7 @@ export function AppRouter() {
             <Routes>
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/selection-wizard" element={<SelectionWizardPage />} />
-                <Route path="/" element={<HomePage />} />
+                <Route path="/" element={<DashboardPage />} />
                 <Route path="/chat" element={<ChatPage />} />
                 <Route path="/onboarding" element={<OnBoardingPage />} />
                 <Route path="/knowledge-base" element={<KnowledgeBasePage />} />
